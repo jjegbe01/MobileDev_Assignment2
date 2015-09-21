65581b9b332df0f73c596dcff2bdf71b4ce6381a
Julian Egbert <jjegbe01@louisville.edu>
2015-09-21 11:01:23 -0400

    Added code to display entered message upon clicking the button

--- MyApplication/app/src/main/java/jjegbe01/assignment2/MainActivity.java
+++ MyApplication/app/src/main/java/jjegbe01/assignment2/MainActivity.java
@@ -6,10 +6,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView mainTextView;
+    EditText mainEditText;
+    Button mainButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +21,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // and then set its text
         mainTextView = (TextView) findViewById(R.id.main_textview);
         mainTextView.setText("Set in Java!");
-        Button mainButton;
         // 2. Access the Button defined in layout XML
         // and listen for it here
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
+        // 3. Access the EditText defined in layout XML
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
 
 
     }
@@ -37,6 +41,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-    mainTextView.setText("Button Pressed");
+        // Take what was typed into the EditText
+        // and use in TextView
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development!");
     }
 }
