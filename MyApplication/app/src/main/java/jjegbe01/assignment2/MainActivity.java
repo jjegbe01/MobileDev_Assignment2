993d0cba51766f6a0db16df2e98a560b201f13aa
Julian Egbert <jjegbe01@louisville.edu>
2015-09-21 10:28:31 -0400

    Added button id and onclicklistener to MA.java

--- MyApplication/app/src/main/java/jjegbe01/assignment2/MainActivity.java
+++ MyApplication/app/src/main/java/jjegbe01/assignment2/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
         // and then set its text
         mainTextView = (TextView) findViewById(R.id.main_textview);
         mainTextView.setText("Set in Java!");
+        Button mainButton;
+        // 2. Access the Button defined in layout XML
+        // and listen for it here
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+
+
     }
 
     @Override
